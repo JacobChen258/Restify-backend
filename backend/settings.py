261bfe6ee47b6c6e80ea2242030cdad6ea1bfc8a
Litao Chen <55116264+JacobChen258@users.noreply.github.com>
2022-04-30 23:15:58 -0400

    update cross origin

--- backend/settings.py
+++ backend/settings.py
@@ -64,7 +64,8 @@ MIDDLEWARE = [
 ]
 
 CORS_ALLOWED_ORIGINS = ['http://localhost:3000','https://restify-frontend.herokuapp.com/','https://www.restify-demo.com/']
-
+CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOW_CREDENTIALS = True
 ROOT_URLCONF = 'backend.urls'
 
 TEMPLATES = [
